f1d509bf7962d1b28fbbd2f9aa4e673487f7f13e
Nicolas <nguiom@unal.edu.co>
2025-02-25 15:50:05 -0500

    registro

--- src/ultrasonic.v
+++ src/ultrasonic.v
@@ -9,8 +9,9 @@ module ultrasonic(clk,reset,echo,trigger,an,seg, enable);
     reg [23:0] counter;
     reg [11:0] cm_cont;
     input enable;
+    reg [15:0] true_cm=0;
 
-    dis Dis(.an(an),.clk(clk),.number(cm),.seg(seg),.rst(reset));
+    dis Dis(.an(an),.clk(clk),.number(true_cm),.seg(seg),.rst(reset));
 
     always @(posedge clk or posedge reset) begin
         if (reset==1) begin
@@ -24,6 +25,7 @@ module ultrasonic(clk,reset,echo,trigger,an,seg, enable);
             if (cm_cont>=2900 && enable==1 && echo==1) begin
                 cm_cont<=0;
                 cm<=cm+1;
+                true_cm<=cm;
             end
             // *Generación del pulso de Trigger (10 µs mínimo)*
             if (counter < 500) begin  // 10 µs con clk de 50 MHz
