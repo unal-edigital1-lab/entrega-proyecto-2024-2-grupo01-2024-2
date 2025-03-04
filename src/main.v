0ac1b4e34b3490461ea51148442b5a0ca9e8496b
Nicolas <nguiom@unal.edu.co>
2025-03-04 12:10:11 -0500

    listos

--- src/main.v
+++ src/main.v
@@ -9,7 +9,7 @@ module main (clk,reset,echo,trigger,an,seg, enable,mosi,sclk,cs,act);
     wire [15:0] true_cm;
     reg [0:11] tiempo=0;
     reg [26:0] contador =0;
-    reg [3:0] state =0;
+    reg [3:0] state =1;
     reg hambre=0;
     output mosi;
     output sclk;
@@ -18,7 +18,7 @@ module main (clk,reset,echo,trigger,an,seg, enable,mosi,sclk,cs,act);
     reg stop=0;
 
     ultrasonic ultra(.clk(clk),.reset(reset),.echo(echo),.trigger(trigger),.an(an),.seg(seg),.enable(enable),.true_cm(true_cm),.otros({tiempo,state}),.stop(stop));
-    //matriz_caras2 caras(.clk(clk),.reset(reset),.state(state),.mosi(mosi),.sclk(sclk),.cs(cs));
+    matriz_caras3 caras(.clk(clk),.reset(reset),.state(state),.mosi(mosi),.sclk(sclk),.cs(cs));
 
     always @(posedge clk or posedge reset) begin
         if (reset==1) begin
@@ -31,18 +31,20 @@ module main (clk,reset,echo,trigger,an,seg, enable,mosi,sclk,cs,act);
                 contador <= 0;
                 tiempo<=tiempo+1;
             end
-            if (tiempo>=60) begin
-                state=1;
+            if (tiempo>=30) begin
+                state<=1;
                 tiempo<=0;
                 stop<=0;
-            end else if(enable==0 && true_cm<=5 && stop==0) begin
+            end else if(enable==0 && true_cm<=5 && stop==0  && state==1) begin
                 tiempo<=0;
                 state<=2;
                 stop<=1;
-            end else if (act[0]==1 && state==2) begin
+            end else if (act[0]==1 && state==2 && enable==1) begin
                 state<=3;
-            end else if(act[1]==1 && state==3)begin
+                stop<=0;
+            end else if(act[1]==1 && state==3 && enable==1)begin
                 state<=0;
+                stop<=0;
             end
         end
     end
